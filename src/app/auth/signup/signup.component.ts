61efdfed5d300a2a247dcbc46c820a0e4b8a36eb
John O'connor <thien.hieu2299@gmail.com>
2023-07-05 20:22:21 +0700

    add signup html

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {  
+  name = new FormControl('');
+  
+
+}
