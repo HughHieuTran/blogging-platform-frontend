0082489e858e5c979f4223c94f591156810440a3
unknown <thien.hieu2299@gmail.com>
2023-07-06 16:05:23 +0700

    hj

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -31,21 +31,25 @@ export class SignupComponent {
   }
 
   signup() {
-    if(!this.signupForm.valid) return;
+    if (!this.signupForm.valid) return;
     this.signupRequestPayload.email = this.signupForm.value.email;
     this.signupRequestPayload.username = this.signupForm.value.username;
-    this.signupRequestPayload.password = this.signupForm.value.password; 
+    this.signupRequestPayload.password = this.signupForm.value.password;
 
     console.log(this.signupRequestPayload);
 
     this.authService.signup(this.signupRequestPayload)
-      .subscribe(data => {
-        this.router.navigate(['/login'],
-          { queryParams: { registered: 'true' } });
-      }, error => {
-        console.log(error);
-        this.toastr.error('Registration Failed! Please try again');
-      });
+      .subscribe({
+        next: (data) => {
+          this.router.navigate(['/login'],
+            { queryParams: { registered: 'true' } })
+        },
+        error: (error) => {
+          console.log(error);
+          this.toastr.error('Registration Failed! Please try again');
+        }
+      })
+   
   }
 
 }
