3d3f22d64390c6054246ba5186fb7d1023dc86e8
unknown <thien.hieu2299@gmail.com>
2023-07-06 15:29:56 +0700

    test signup html

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -31,19 +31,21 @@ export class SignupComponent {
   }
 
   signup() {
+    if(!this.signupForm.valid) return;
     this.signupRequestPayload.email = this.signupForm.value.email;
     this.signupRequestPayload.username = this.signupForm.value.username;
     this.signupRequestPayload.password = this.signupForm.value.password; 
 
     console.log(this.signupRequestPayload);
-    // this.authService.signup(this.signupRequestPayload)
-    //   .subscribe(data => {
-    //     this.router.navigate(['/login'],
-    //       { queryParams: { registered: 'true' } });
-    //   }, error => {
-    //     console.log(error);
-    //     this.toastr.error('Registration Failed! Please try again');
-    //   });
+
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(data => {
+        this.router.navigate(['/login'],
+          { queryParams: { registered: 'true' } });
+      }, error => {
+        console.log(error);
+        this.toastr.error('Registration Failed! Please try again');
+      });
   }
 
 }
